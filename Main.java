f88c0b488e10fdce762c74911011a6968424a5f9
AkashArasppan <140300026+AkashArasppan@users.noreply.github.com>
2023-08-02 10:34:45 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+class test {
+    public static void main(String[] args) {
+        byte A=100;
+        short B=200;
+        int price=30000;
+        long mobile=9585435838L;
+        System.out.println(A);
+        System.out.println(B);
+        System.out.println(price);
+        System.out.println(mobile);
+
+    }
+}
